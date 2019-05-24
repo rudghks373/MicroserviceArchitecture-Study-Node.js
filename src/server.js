df7fcbb0b7bfa1e79315222edac7d79805568fd2
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-24 13:32:23 +0900

    Updata Server

--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
 "use strict";
-
 const net = require("net");
 const tcpClient = require("./client.js");
 
+/* * 서버 클래스 */
 class tcpServer {
   constructor(name, port, urls) {
-    // ➊ 생성자
-
+    //서버 상태 정보
     this.context = {
-      // ➋ 서버 정보
       port: port,
       name: name,
       urls: urls
     };
     this.merge = {};
+
+    // 서버 객체 생성
     this.server = net.createServer(socket => {
-      // ➌ 서버 생성
-      this.onCreate(socket); // ➍ 클라이언트 접속 이벤트 처리
+      // 클라이언트 접속 이벤트
+      this.onCreate(socket);
+
+      // 에러 이벤트
       socket.on("error", exception => {
-        // ➎ 에러 이벤트 처리
         this.onClose(socket);
       });
+
+      // 클라이언트 접속 종료 이벤트
       socket.on("close", () => {
-        // ➏ 클라이언트 접속 종료 이벤트 처리
         this.onClose(socket);
       });
+
+      // 데이터 수신 이벤트
       socket.on("data", data => {
-        // ➐ 데이터 수신 처리
         var key = socket.remoteAddress + ":" + socket.remotePort;
         var sz = this.merge[key]
           ? this.merge[key] + data.toString()
@@ -45,12 +48,13 @@ class tcpServer {
       });
     });
 
+    // 서버 객체 에러 이벤트
     this.server.on("error", err => {
-      // ➑ 서버 객체 에러 처리
       console.log(err);
     });
+
+    // 리슨
     this.server.listen(port, () => {
-      // ➒ 리슨
       console.log("listen", this.server.address());
     });
   }
@@ -62,40 +66,47 @@ class tcpServer {
   onClose(socket) {
     console.log("onClose", socket.remoteAddress, socket.remotePort);
   }
-}
 
-onClose(socket) {
-  console.log("onClose", socket.remoteAddress, socket.remotePort);
-}
-  
-connectToDistributor(host, port, onNoti) {    // ➊ Distributor 접속 함수
-var packet = {                            // ➋ Distributor에 전달할 패킷 정의
-  uri: "/distributes",
-  method: "POST",
-  key: 0,
-  params: this.context
-};
-var isConnectedDistributor = false;       // ➌ Distributor 접속 상태
-  
-this.clientDistributor = new tcpClient(   // ➍ Client 클래스 인스턴스 생성
-  host
-  , port
-  , (options) => {                      // ➎ 접속 이벤트
-      isConnectedDistributor = true;
-      this.clientDistributor.write(packet);
-  }
-  , (options, data) => { onNoti(data); }             // ➏ 데이터 수신 이벤트
-  , (options) => { isConnectedDistributor = false; } // ➐ 접속 종료 이벤트
-  , (options) => { isConnectedDistributor = false; } // ➑ 에러 이벤트
-  );
+  // Distributor 접속 함수
+  connectToDistributor(host, port, onNoti) {
+    // Distributor 전달 패킷
+    var packet = {
+      uri: "/distributes",
+      method: "POST",
+      key: 0,
+      params: this.context
+    };
+    var isConnectedDistributor = false;
 
-  setInterval(() => {                       // ➒ 주기적인 Distributor 접속 시도
-      if (isConnectedDistributor /= true) {
-          this.clientDistributor.connect();
+    this.clientDistributor = new tcpClient(
+      host,
+      port,
+      options => {
+        // Distributor 접속 이벤트
+        isConnectedDistributor = true;
+        this.clientDistributor.write(packet);
+      },
+      // Distributor 데이터 수신 이벤트
+      (options, data) => {
+        onNoti(data);
+      },
+      // Distributor 접속 종료 이벤트
+      options => {
+        isConnectedDistributor = false;
+      },
+      // Distributor 통신 에러 이벤트
+      options => {
+        isConnectedDistributor = false;
       }
-  }, 3000);
-}
-}
+    );
 
+    // 주기적으로 재접속 시도
+    setInterval(() => {
+      if (isConnectedDistributor != true) {
+        this.clientDistributor.connect();
+      }
+    }, 3000);
+  }
+}
 
 module.exports = tcpServer;
