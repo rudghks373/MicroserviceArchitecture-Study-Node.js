84e4d0acfecbb8d369dd6456d258fbff801a4fd7
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-06-13 14:46:26 +0900

    Updata text

--- src/server.js
+++ src/server.js
@@ -5,28 +5,28 @@ const tcpClient = require("./client.js");
 
 class tcpServer {
   constructor(name, port, urls) {
-    // ➊ 생성자
+    // 1 생성자
 
     this.context = {
-      // ➋ 서버 정보
+      // 2 서버 정보
       port: port,
       name: name,
       urls: urls
     };
     this.merge = {};
     this.server = net.createServer(socket => {
-      // ➌ 서버 생성
-      this.onCreate(socket); // ➍ 클라이언트 접속 이벤트 처리
+      // 3 서버 생성
+      this.onCreate(socket); // 4 클라이언트 접속 이벤트 처리
       socket.on("error", exception => {
-        // ➎ 에러 이벤트 처리
+        // 5 에러 이벤트 처리
         this.onClose(socket);
       });
       socket.on("close", () => {
-        // ➏ 클라이언트 접속 종료 이벤트 처리
+        // 6 클라이언트 접속 종료 이벤트 처리
         this.onClose(socket);
       });
       socket.on("data", data => {
-        // ➐ 데이터 수신 처리
+        // 7 데이터 수신 처리
         var key = socket.remoteAddress + ":" + socket.remotePort;
         var sz = this.merge[key]
           ? this.merge[key] + data.toString()
@@ -46,11 +46,11 @@ class tcpServer {
     });
 
     this.server.on("error", err => {
-      // ➑ 서버 객체 에러 처리
+      // 8 서버 객체 에러 처리
       console.log(err);
     });
     this.server.listen(port, () => {
-      // ➒ 리슨
+      // 9 리슨
       console.log("listen", this.server.address());
     });
   }
